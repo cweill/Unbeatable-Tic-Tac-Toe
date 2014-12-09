b07f9abb2649a0f80553ded7857e93aa3d974b74
Charles Weill <cew225@cornell.edu>
2014-12-08 23:01:28 -0500

    Initial Commit

---
+++ UnbeatableTicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnbeatableTicTacToe
+//
+//  Created by Charlie Weill on 12/8/14.
+//  Copyright (c) 2014 Charlie Weill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnbeatableTicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnbeatableTicTacToe
+//
+//  Created by Charlie Weill on 12/8/14.
+//  Copyright (c) 2014 Charlie Weill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
